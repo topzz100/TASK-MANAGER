9237a5ac3f7fb9f10b5470c557effe80c91dea59
topzz100 <adenekantope@gmail.com>
2021-10-31 21:42:02 +0100

    error

--- controller/tasks.js
+++ controller/tasks.js
@@ -6,15 +6,17 @@ const getAllTasks = async(req, res) => {
   const tasks = await Task.find()
   res.status(200).send(tasks)
 }
-const addTask = (req, res) => {
- const task = Task.create(req.body)
- res.status(200).send(task)
+const addTask = async(req, res) => {
+ const task = await Task.create(req.body)
+ res.status(200).json(task)
 }
 const getTask = (req, res) => {
 
 }
 const deleteTask = (req, res) => {
-
+  
+const task =Task.findOneAndDelete({_id : req.params.id})
+res.status(200).send(req.params.id)
 }
 const updateTask = (req, res) => {
 
