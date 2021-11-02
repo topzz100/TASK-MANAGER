aa110e7b3552783ba8e63dfdb1f3239263897004
topzz100 <adenekantope@gmail.com>
2021-11-02 10:41:05 +0100

    errors handled

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,51 +1,51 @@
 // const express = require('express')
 const Task = require('../database/models/task')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/customError')
 
 
-const getAllTasks = async(req, res) => {
+const getAllTasks = asyncWrapper(async(req, res) => {
   const tasks = await Task.find()
   res.status(200).send(tasks)
-}
-const addTask = async(req, res) => {
-  try{
-    const task = await Task.create(req.body)
+})
+
+
+const addTask =  asyncWrapper(async(req, res) => {
+   const task = await Task.create(req.body)
     res.status(200).json({task})
-  }catch(err){
-    console.log(err)
-  }
+})
  
-}
 
-const getTask = async(req, res) => {
-  try{
-    const task = await Task.findOne({_id : req.params.id})
-    res.status(200).json(task)
-  }catch(err){
-    res.send(err)
-  }
-}
 
-const deleteTask = async(req, res) => {  
-  try{
-    const task = await Task.findOneAndDelete({_id :  req.params.id})
-    res.status(200).json({task})
-  }catch(err){
-    res.send(err)
+const getTask = asyncWrapper(async(req, res) => {
+  const task = await Task.findOne({_id : req.params.id})
+  if (!task) {
+  return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
 
-}
+  res.status(200).json({ task })  
+}) 
 
-const updateTask = async(req, res) => {
-   try{
-    const task = await Task.findOneAndUpdate({_id: req.params.id}, req.body, {
+const deleteTask = asyncWrapper(async(req, res) => {
+  const task = await Task.findOneAndDelete({_id :  req.params.id})
+  if (!task) {
+  return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+
+  res.status(200).json({ task })    
+})
+
+const updateTask = asyncWrapper(async(req, res) => {
+ const task = await Task.findOneAndUpdate({_id: req.params.id}, req.body, {
       new: true,
       runValidators: true
     })
-    res.status(200).json({task})
-  }catch(err){
-    console.log(err)
+  if (!task) {
+  return next(createCustomError(`No task with id : ${taskID}`, 404))
   }
-}
+
+  res.status(200).json({ task })  
+})
 
 
 
--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const connectDB = require('./database/db')
+const notFound = require('./middleware/notFound');
+const errorHandlerMiddleware = require('./middleware/errorHandler');
 
 const PORT = process.env.PORT || 3000
+
+
 
 app.use(express.json())
 
 //routes
 app.use('/api/v1/tasks', require('./routes/tasks'))
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-// app.listen(PORT, ()=> console.log(`app is listening in port ${PORT}`))
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 const connect = async() => {
   try{
     await connectDB(process.env.MONGO_URL)
