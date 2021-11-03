969845bbf7d61e6942aa8575264e8fc18edf0d79
topzz100 <adenekantope@gmail.com>
2021-11-03 20:48:34 +0100

    UI in progress

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const errorHandlerMiddleware = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3000
 
 
-
+app.use(express.static('./public'));
 app.use(express.json())
 
 //routes
